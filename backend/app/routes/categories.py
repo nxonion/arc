32b10a1117b4beb1026461f31d00e05407921558
nxonion <defenderonion@gmail.com>
2026-07-24 16:08:34 +0800

    add routes

---
+++ backend/app/routes/categories.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from typing import List
+from backend.app.services.category_service import CategoryService
+from ..database import get_db
+from ..schemas.category import CategoryResponse
+
+router = APIRouter(prefix="/api/categories", tags=["categories"])
+
+
+@router.get("", response_model=List[CategoryResponse], status_code=status.HTTP_200_OK)
+def get_categories(db: Session = Depends(get_db)):
+    service = CategoryService(get_db) # pyright: ignore[reportArgumentType]
+    return service.get_all_categories()
+
+
+@router.get(
+    "/{category_id}", response_model=CategoryResponse, status_code=status.HTTP_200_OK
+)
+def get_category(category_id: int, db: Session = Depends(get_db)):
+    service = CategoryService(db)
+    return service.get_category_by_id(category_id)
+
